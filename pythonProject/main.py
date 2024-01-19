01b3e0327c7111da593d2479243103d57e31682f
shivendra <shivendrakeshari11@gmail.com>
2024-01-19 18:50:19 +0530

    Opencv-python
    Made a two project related to computer vision Object detection and face detection

---
+++ pythonProject/main.py
@@ -0,0 +1,6 @@
+from tkinter import *
+window=Tk()
+window.geometry("300 * 300")
+window.title("welcome")
+Label1=Label(window,text="welcome to 3c",font=("axial",16,"bold") ).pack()
+window.mainloop()
